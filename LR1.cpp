bb684b3ec8d6bce50e70f81f515a7bd04cdbb39e
svetosh <70635136+svetosh@users.noreply.github.com>
2020-10-09 17:41:59 +0300

    Update LR1.cpp

--- LR1.cpp
+++ LR1.cpp
@@ -6,21 +6,24 @@ int main()
 {
 	std::cout << "Hello!\n";
 
-
-	int x = 0;
+	//1
+	 double x = 0.0;
 	std::cout << "Equation is 8x^2 + x^4 - 1 \nEnter x value: ";							
 	std::cin >> x;
-		long int result = 8 * pow(x, 7) + pow(x, 4) - 1;
-	std::cout << "\nThe result is " << result;
-	//
+	double result = 8 * pow(x, 2) + pow(x, 4) - 1;
+	std::cout << std::fixed;
+	std::cout.precision(3);
+	std::cout << "\nOtvet " << result;
+
+	//2
 
 	int a;
 	std::cout << "\nEquation is 3*|a|-2*(a+6)^1/2  \nEnter a value: ";					 
 	std::cin >> a;
-		long int resultq = 3 * abs(a) - 2 * sqrt(a + 6);
-	std::cout << "\nThe result is " << resultq;
+		int resultq = 3 * abs(a) - 2 * sqrt(a + 6);
+	std::cout << "\nThe result is " << std::showpos << resultq;
 
-	//
+	//3
 	bool m = 0,						
 		n = 0,
 		l = 0,
@@ -34,16 +37,16 @@ int main()
 	std::cout << "Enter k value: ";
 	std::cin >> k;
 		long int resultw = (m xor n) && ((!l) || k);
-	std::cout << "\nThe result is " << resultw;
+	std::cout << "\nThe result is " << std::boolalpha << resultw;
 
-	//
+	//4
 	int b = 0;						
 	std::cout << "\nEquation is 2^b \nEnter b value: ";							 
 	std::cin >> b;
 		long int resulte = pow(2, b);
-	std::cout << "\nThe result is " << std::dec << resulte << "\nThe result in oct " << std::oct << resulte;
+	std::cout << "\nThe result is " << std::dec << std::noshowpos << resulte << "\nThe result in oct " << std::oct << resulte;
 
-	//
+	//5
 	unsigned short int f = 0,		 
 		c = 0,
 		e = 0,
@@ -56,7 +59,7 @@ int main()
 	std::cin >> e;
 	std::cout << "Enter d value: ";
 	std::cin >> d;
-		long int resultr = (f) | (~(c & (((~e) & d) | ((~d) & e))));
-	std::cout << "\nThe result is " << std::hex << resultr;														 
+		unsigned short int resultr = (f) | (~(c & (((~e) & d) | ((~d) & e))));
+	std::cout << "\nThe result is " << std::setw(6) << std::setfill('0') << std::internal << std::hex << std::showbase << resultr;
 	return 0;
 }
